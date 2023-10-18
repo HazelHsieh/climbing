7969e13d40a9b1927aaaa9fd8367d5272e0100ac
Hazel <owo0726@gmail.com>
2023-10-18 16:01:32 +0800

    fix: PageHeader undefined

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
 const props = defineProps({
   imageUrl: {
     type: String,
@@ -10,12 +13,12 @@ const props = defineProps({
 <template>
   <div class="flex flex-col">
     <div class="overflow-hidden relative">
-      <img :src="imageUrl" class="w-full h-[200px] lg:h-[400px] object-cover" :alt="this.$route.meta.title" />
+      <img :src="imageUrl" class="w-full h-[200px] lg:h-[400px] object-cover" :alt="route.meta.title" />
     </div>
     <div class="container">
       <div class="relative z-10 flex items-center justify-center mx-auto w-full max-w-[400px] -translate-y-1/2 bg-base-100 dark:bg-dark_mode_1">
         <p class="relative px-6 py-2 after:content-[''] after:relative after:-bottom-2 after:block after:h-1 after:bg-text_color dark:after:bg-white text-center text-2xl lg:text-4xl font-bold">
-          {{ this.$route.meta.title }}
+          {{ route.meta.title }}
         </p>
       </div>
     </div>
