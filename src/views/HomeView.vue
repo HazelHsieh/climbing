10f682491d51edf11d71d0caec06c52fb2513424
Hazel <owo0726@gmail.com>
2023-10-20 17:12:36 +0800

    feat: category Images hover effect and homeView scrolling effect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <script setup>
 // import { ref, onMounted } from "vue";
-
+import { ref } from "vue";
 import { useRouter } from "vue-router";
+import category1Image from "@/assets/images/Mountain/郡大山01.jpg";
+import category1HoverImage from "@/assets/images/Mountain/郡大山03.png";
+import category2Image from "@/assets/images/Mountain/奇萊南峰01.png";
+import category2HoverImage from "@/assets/images/Mountain/奇萊南峰02.jpg";
+import category3Image from "@/assets/images/Mountain/嘉明湖01.png";
+import category3HoverImage from "@/assets/images/Mountain/嘉明湖02.jpg";
 
 const router = useRouter();
 const toCategory = (tag) => {
@@ -10,7 +16,14 @@ const toCategory = (tag) => {
     query: { category: tag },
   });
 };
-// console.log(toCategory);
+
+const isHovering = ref({ 1: false, 2: false, 3: false });
+
+const setHovering = (item) => {
+  for (let i = 1; i <= 3; i++) {
+    isHovering.value[i] = i === item;
+  }
+};
 
 // import Loading from "vue-loading-overlay";
 
@@ -46,15 +59,15 @@ const toCategory = (tag) => {
     <p class="text-xl text-center pb-5">探索不同體驗</p>
     <ul class="flex flex-wrap gap-10">
       <li class="flex flex-col sm:flex-row justify-center items-center">
-        <div class="w-full sm:w-1/2 block sm:hidden relative">
+        <div data-aos="fade-left" data-aos-duration="1000" class="w-full sm:w-1/2 block sm:hidden relative">
           <!-- 1. -->
           <!-- <figure class="p-1 border-2 border-black-400 rounded-md">
             <img class="rounded-md" src="@/assets/images/Mountain/郡大山01.jpg" alt="郡大山01">
           </figure> -->
           <!-- 2. -->
-          <div class="card shadow-xl rounded-sm">
+          <div @mouseenter="setHovering(1)" @mouseleave="setHovering(0)" class="card shadow-xl rounded-sm">
             <figure class="pr-5 pb-3">
-              <img src="@/assets/images/Mountain/郡大山01.jpg" alt="郡大山01" class="rounded-sm" />
+              <img :src="isHovering[1] ? category1HoverImage : category1Image" class="rounded-sm" alt="郡大山01" />
             </figure>
             <!-- <div class="">
               <h2 class="text-end text-2xl mr-5">輕鬆爬爬</h2>
@@ -64,7 +77,7 @@ const toCategory = (tag) => {
             <p class="text-end mr-5 text-2xl text-black/70 bg-white/50 px-2 shadow-md">輕鬆爬爬</p>
           </div>
         </div>
-        <div class="w-full sm:w-1/2 p-5">
+        <div data-aos="fade-right" data-aos-duration="1000" class="w-full sm:w-1/2 p-5">
           <p class="px-1 tracking-wide">輕鬆爬爬，是一場輕鬆而充實的山行體驗。無需花費太多的時間，半天至一天的行程即可完成。這是一個適合所有人的選擇，不論你是初次嘗試登山，還是希望度過一個輕鬆的週末。在輕鬆爬爬中，你可以欣賞到大自然的美麗風景，同時也感受到輕鬆的快樂。</p>
           <br />
           <!-- <div class="flex items-end">
@@ -75,15 +88,15 @@ const toCategory = (tag) => {
             <span class="material-icons text-sm group-hover:bg-sky-50 hidden group-hover:block group-hover:bg-sky-50 group-hover:rounded group-hover:delay-150">trending_flat</span>
           </a>
         </div>
-        <div class="w-full sm:w-1/2 hidden sm:block relative">
+        <div data-aos="fade-left" data-aos-duration="1000" class="w-full sm:w-1/2 hidden sm:block relative">
           <!-- 1. -->
           <!-- <figure class="p-1 border-2 border-black-400 rounded-md">
             <img class="rounded-md" src="@/assets/images/Mountain/郡大山01.jpg" alt="郡大山01">
           </figure> -->
           <!-- 2. -->
-          <div class="card shadow-xl rounded-sm">
+          <div @mouseenter="setHovering(1)" @mouseleave="setHovering(0)" class="card shadow-xl rounded-sm">
             <figure class="pr-5 pb-3">
-              <img src="@/assets/images/Mountain/郡大山01.jpg" alt="郡大山01" class="rounded-sm" />
+              <img :src="isHovering[1] ? category1HoverImage : category1Image" alt="郡大山01" class="rounded-sm" />
             </figure>
             <!-- <div class="">
               <h2 class="text-end text-2xl mr-5">輕鬆爬爬</h2>
@@ -95,17 +108,17 @@ const toCategory = (tag) => {
         </div>
       </li>
       <li class="flex flex-col sm:flex-row justify-center items-center">
-        <div class="w-full sm:w-1/2 relative">
-          <div class="card shadow-xl rounded-sm">
+        <div data-aos="fade-right" data-aos-duration="2000" class="w-full sm:w-1/2 relative">
+          <div @mouseenter="setHovering(2)" @mouseleave="setHovering(0)" class="card shadow-xl rounded-sm">
             <figure class="pr-5 pb-3">
-              <img src="@/assets/images/Mountain/奇萊南峰01.png" alt="奇萊南峰01" class="rounded-sm" />
+              <img :src="isHovering[2] ? category2HoverImage : category2Image" alt="奇萊南峰01" class="rounded-sm" />
             </figure>
           </div>
           <div class="absolute bottom-0 right-4 z-10">
             <p class="text-end mr-5 text-2xl text-black/70 bg-white/50 px-2 shadow-md">流汗爬爬</p>
           </div>
         </div>
-        <div class="w-full sm:w-1/2 p-5">
+        <div data-aos="fade-left" data-aos-duration="2000" class="w-full sm:w-1/2 p-5">
           <p class="px-5 tracking-wide">流汗爬爬，給你帶來更多的挑戰和冒險。除了一天的行程外，你還可以選擇兩天一夜來享受山區的美景。這條路線可能需要更多的體力，但你會發現，汗水是通向美麗景點的代價。流汗爬爬讓你有機會欣賞不同的風景，同時也留下了寶貴的回憶。</p>
           <br />
           <a href="" @click.prevent="toCategory('流汗爬爬')" class="group flex justify-end items-end text-end mr-7">
@@ -116,17 +129,17 @@ const toCategory = (tag) => {
         </div>
       </li>
       <li class="flex flex-col sm:flex-row justify-center items-center">
-        <div class="w-full sm:w-1/2 block sm:hidden relative">
-          <div class="card shadow-sm rounded-sm">
+        <div data-aos="fade-left" data-aos-duration="2000" class="w-full sm:w-1/2 block sm:hidden relative">
+          <div @mouseenter="setHovering(3)" @mouseleave="setHovering(0)" class="card shadow-sm rounded-sm">
             <figure class="pr-5 pb-3">
-              <img src="@/assets/images/Mountain/嘉明湖11.png" alt="嘉明湖11" class="rounded-sm" />
+              <img :src="isHovering[3] ? category3HoverImage : category3Image" alt="嘉明湖1" class="rounded-sm" />
             </figure>
           </div>
           <div class="absolute bottom-0 right-4 z-10">
             <p class="text-end mr-5 text-2xl text-black/70 bg-white/50 px-2 shadow-md">認真爬爬</p>
           </div>
         </div>
-        <div class="w-full sm:w-1/2 p-5">
+        <div data-aos="fade-right" data-aos-duration="2000" class="w-full sm:w-1/2 p-5">
           <p class="px-5 tracking-wide">認真爬爬，是真正的登山冒險。這需要更多的時間和耐力，可能需要三天兩夜或更長的時間。在這條路線上，你需要更多的計劃和籌備，同時也需要更多的登山經驗。然而，這個旅程也讓你有機會探索更遙遠的地方，並感受到大自然的震撼和寧靜。</p>
           <br />
           <a href="" @click.prevent="toCategory('認真爬爬')" class="group flex justify-end items-end text-end mr-7">
@@ -135,10 +148,10 @@ const toCategory = (tag) => {
             <span class="material-icons text-sm group-hover:bg-sky-50 hidden group-hover:block group-hover:bg-sky-50 group-hover:rounded group-hover:delay-150">trending_flat</span>
           </a>
         </div>
-        <div class="w-full sm:w-1/2 hidden sm:block relative">
-          <div class="card shadow-sm rounded-sm">
+        <div data-aos="fade-left" data-aos-duration="2000" class="w-full sm:w-1/2 hidden sm:block relative">
+          <div @mouseenter="setHovering(3)" @mouseleave="setHovering(0)" class="card shadow-sm rounded-sm">
             <figure class="pr-5 pb-3">
-              <img src="@/assets/images/Mountain/嘉明湖11.png" alt="嘉明湖11" class="rounded-sm" />
+              <img :src="isHovering[3] ? category3HoverImage : category3Image" alt="嘉明湖1" class="rounded-sm" />
             </figure>
           </div>
           <div class="absolute bottom-0 right-4 z-10">
@@ -150,7 +163,7 @@ const toCategory = (tag) => {
   </section>
   <section class="container mb-10 relative">
     <img class="absolute top-0 -left-[25%]" src="@/assets/images/bg.png" alt="" />
-    <div class="flex container">
+    <div data-aos="fade-up" data-aos-duration="2000" class="flex container">
       <p class="text-2xl w-1/12 mr-1 z-10 dark:text-shadow">熱門<br />行程</p>
       <ul class="w-11/12 flex flex-col md:flex-row gap-4">
         <li class="card bg-base-100 dark:bg-dark_mode_2 border border-1 border-primary rounded-sm">
@@ -207,25 +220,25 @@ const toCategory = (tag) => {
     <p class="text-xl text-center mb-5">你還想知道什麼嗎？</p>
     <p class="text-center mb-5">透過以下分類讓你快速找到需要的登山知識</p>
     <ul class="list-none flex flex-wrap text-center p-10 font-bold">
-      <li class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-primary hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/wear.jpg')] text-white mb-2 shadow-md">
+      <li data-aos="fade-down-right" data-aos-duration="2000" class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-primary hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/wear.jpg')] text-white mb-2 shadow-md">
         <figure class="flex flex-col items-center justify-center">
           <img class="" src="@/assets/images/icons/wearIcon-w.svg" alt="wearIcon-w" />
           <p class="">身著推薦</p>
         </figure>
       </li>
-      <li class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-white hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/food.webp')] text-black mt-2 shadow-md">
+      <li data-aos="fade-down-left" data-aos-duration="2000" class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-white hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/food.webp')] text-black mt-2 shadow-md">
         <figure class="flex flex-col items-center justify-center">
           <img class="" src="@/assets/images/icons/foodIcon-b.svg" alt="wearIcon-w" />
           <p class="">糧食推薦</p>
         </figure>
       </li>
-      <li class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-primary hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/smallThings.jpg')] text-white mb-2 shadow-md">
+      <li data-aos="fade-up-right" data-aos-duration="2000" class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-primary hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/smallThings.jpg')] text-white mb-2 shadow-md">
         <figure class="flex flex-col items-center justify-center">
           <img class="" src="@/assets/images/icons/smallThingsIcon-w.svg" alt="smallThingsIcon-w" />
           <p class="">行動小物</p>
         </figure>
       </li>
-      <li class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-white hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/fitOut1.jpg')] text-black mt-2 shadow-md">
+      <li data-aos="fade-up-left" data-aos-duration="2000" class="w-full xs:w-1/2 sm:w-1/4 aspect-square flex items-center justify-center bg-white hover:bg-cover hover:bg-no-repeat hover:bg-center hover:bg-[url('@/assets/images/blog/fitOut1.jpg')] text-black mt-2 shadow-md">
         <figure class="flex flex-col items-center justify-center">
           <img class="" src="@/assets/images/icons/fitOutIcon-b.svg" alt="foodIcon-w" />
           <p class="">裝備推薦</p>
